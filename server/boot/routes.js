54335dcf7f40bfeacdd3d97d7d68b7127fc4fc5c
Sebastián Vicencio <sivicencio@gmail.com>
2017-05-17 00:27:18 -0400

    Add upload controller/routes to handle Excel uploads

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,9 +1,13 @@
 'use strict';
 
+var uploadController = require('../controllers/upload-controller');
+
 module.exports = function(app) {
   var router = app.loopback.Router();
-  router.get('/ping', function(req, res) {
-    res.send('pong');
-  });
+
+  router.get('/upload', uploadController.uploadCreateGet);
+
+  router.post('/upload', uploadController.uploadCreatePost);
+
   app.use(router);
-}
+};
