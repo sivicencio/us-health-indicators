b236c43873fb8b704d8fbac078d8eabb7184739a
Sebastián Vicencio <sivicencio@gmail.com>
2017-05-20 21:44:03 -0400

    Add empty details instance remote method to County

--- common/models/county.js
+++ common/models/county.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function(County) {
+  /**
+   * Get county details including indicators
+   * @param {Function(Error)} callback
+   */
 
+  County.prototype.details = function(callback) {
+    // TODO
+    callback(null);
+  };
 };
