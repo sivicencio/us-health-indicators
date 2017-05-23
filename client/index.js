849d7585aff506eb707299c7073b6c9606f2c814
Sebastián Vicencio <sivicencio@gmail.com>
2017-05-23 02:30:41 -0400

    Add TitleComponent with basic logic using it as index h1 title

--- client/index.js
+++ client/index.js
@@ -1,1 +1,8 @@
-console.log('Initial message');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TitleComponent from './components/TitleComponent';
+
+ReactDOM.render(
+  <TitleComponent titleText="US Health Indicators" />,
+  document.getElementById('root')
+);
