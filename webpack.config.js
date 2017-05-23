d2e7aabd061fb5add682f49334006d3ff0ee055c
Sebastián Vicencio <sivicencio@gmail.com>
2017-05-23 01:28:49 -0400

    Add webpack package with webpack.config file including config for dev
    Use basic index.js as working example

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/index'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+};
