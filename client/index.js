9e353286aa1b3624ebaa5f5cd39a05d0d7487162
Sebastián Vicencio <sivicencio@gmail.com>
2017-05-30 01:48:56 -0400

    Add promise-polyfill and setasap packages including them at index.js for browser support

--- client/index.js
+++ client/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Promise from 'promise-polyfill';
+import setAsap from 'setasap';
 import AppComponent from './components/AppComponent';
+
+Promise._immediateFn = setAsap;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 
 ReactDOM.render(
   <AppComponent />,
