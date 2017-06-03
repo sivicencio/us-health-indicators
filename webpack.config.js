d5898e8658047de43199f3ffc787033662f38fa2
Sebastián Vicencio <sivicencio@gmail.com>
2017-06-03 02:10:13 -0400

    Add css-loader and style-loader packages and configure webpack with them

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,17 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      }, {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
       }
     ]
   },
