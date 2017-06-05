f74be594762d03e39ad3bd30fe7cf33ed2b30679
Sebastián Vicencio <sivicencio@gmail.com>
2017-06-04 23:18:19 -0400

    Add searchTerm to App state and update state using new Search component change
    Also pass searchTerm as prop to CountyListContainer and filter name/state based on that prop value

---
+++ client/components/Search.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import styles from '../public/stylesheets/search.css';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onSearchChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className={ styles.search }>
+        <fieldset>
+          <input onChange={ this.handleChange } placeholder="Enter county or state..." />
+        </fieldset>
+      </div>
+    );
+  }
+}
+
+export default Search;
--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MainPage from '../components/MainPage';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+import Search from '../components/Search';
 import CountyListContainer from './CountyListContainer';
 import CountyDetailContainer from './CountyDetailContainer';
 
@@ -9,11 +10,16 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleCountyClick = this.handleCountyClick.bind(this);
-    this.state = { selectedCounty: '' };
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.state = { selectedCounty: '', searchTerm: '' };
   }
 
   handleCountyClick(countyId) {
     this.setState({ selectedCounty: countyId });
+  }
+
+  handleSearchChange(term) {
+    this.setState({ searchTerm: term })
   }
 
   render() {
@@ -21,7 +27,8 @@ class App extends Component {
       <MainPage>
         <Sidebar>
           <Header title="US Health Indicators" />
-          <CountyListContainer onCountyClick={ this.handleCountyClick } />
+          <Search onSearchChange={ this.handleSearchChange }/>
+          <CountyListContainer onCountyClick={ this.handleCountyClick } searchTerm={ this.state.searchTerm }/>
         </Sidebar>
         <CountyDetailContainer countyId={ this.state.selectedCounty } />
       </MainPage>
