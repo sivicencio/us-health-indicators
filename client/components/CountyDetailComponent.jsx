81e47729f92d80a60d4e3f34d5b06023bb0db06f
Sebastián Vicencio <sivicencio@gmail.com>
2017-06-06 03:13:12 -0400

    Add renderChart method to CountyDetailComponent

--- client/components/CountyDetailComponent.jsx
+++ client/components/CountyDetailComponent.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Chart } from 'react-google-charts';
 import styles from '../public/stylesheets/counties.css';
 
 class CountyDetailComponent extends Component {
   constructor(props) {
     super(props);
     this.renderLatestMetric = this.renderLatestMetric.bind(this);
+    this.renderChart = this.renderChart.bind(this);
   }
 
   renderLatestMetric(countyIndicator) {
@@ -42,7 +44,61 @@ class CountyDetailComponent extends Component {
             { county.countyIndicators.map(this.renderLatestMetric) }
           </ul>
         </div>
+
+        { this.renderChart() }
       </div>
+    );
+  }
+
+  renderChart() {
+    const countyIndicators = this.props.county.countyIndicators;
+    let columns = [
+      { type: 'string', label: 'Year' }
+    ];
+
+    columns = columns.concat(countyIndicators
+          .filter((countyIndicator) => countyIndicator.metrics.length > 0)
+          .map((countyIndicator) => (
+            { type: 'number', label: countyIndicator.indicator.name }
+          )));
+
+    const simplifiedIndicators = countyIndicators.map((countyIndicator) => (
+      countyIndicator.metrics
+        .concat()
+        .sort((a, b) => a.year - b.year)
+        .map((metric) => (
+          [ metric.year.toString(), metric.peopleNumber ]
+        ))
+    ));
+
+    // Assumption: if an indicator has metrics, then it covers from 2004 to 2013
+    // This way we treat arrays assuming they have the same length
+    let rows = simplifiedIndicators[0];
+
+    for(let i = 1; i < simplifiedIndicators.length; i++) {
+      simplifiedIndicators[i].forEach((metric, index) => (
+        (rows[index]).push(metric[1])
+      ));
+    }
+
+    return (
+      <Chart
+        chartType="LineChart"
+        rows={ rows }
+        columns={ columns }
+        options={{
+          title: 'US indicators prevalence evolution between 2004 and 2013',
+          hAxis: {
+            title: 'Year'
+          },
+          vAxis: {
+            title: 'People number'
+          }
+        }}
+        width="100%"
+        height="400px"
+        legend_toggle
+      />
     );
   }
 }
